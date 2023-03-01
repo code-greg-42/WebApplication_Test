b7e1c703889ec7c5d8f04b04f3a4e2dc304d7dff
greg <grandersson1991@gmail.com>
2023-03-01 14:43:54 -0500

    second commit -- added required content

--- WebApplication_Test/wwwroot/js/site.js
+++ WebApplication_Test/wwwroot/js/site.js
@@ -2,3 +2,23 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+var id = null;
+function myMove() {
+    console.log('move starting!');
+    var elem = document.getElementById("ani-start");
+    var pos = 0;
+    var szeX = 200;
+    clearInterval(id);
+    id = setInterval(frame, 3);
+    function frame() {
+        if (pos == 300) {
+            clearInterval(id);
+        } else {
+            pos++;
+            szeX += 2;
+            elem.style.top = pos + 'px';
+            elem.style.width = szeX + 'px';
+        }
+    }
+}
